94c8c444204b608f54a58078dbda651f765f7b06
Savanna <pinkoski@msu.edu>
2017-04-29 15:24:59 -0400

    function forbutton map

---
+++ app/src/main/java/edu/msu/pinkoski/project3_team16/DetailActivity.java
@@ -0,0 +1,21 @@
+package edu.msu.pinkoski.project3_team16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Savanna on 4/29/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+
+
+
+
+
+    public void buttonMap(View view){
+
+
+
+    }
+}
