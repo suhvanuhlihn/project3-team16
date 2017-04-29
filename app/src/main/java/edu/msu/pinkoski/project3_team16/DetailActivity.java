ab5c8a28ed8b31a6da9ef91907f6b5c87abfb5c1
by-the-w3i <jiangw14@msu.edu>
2017-04-29 16:14:21 -0400

    add clickable btn for detail activity

--- app/src/main/java/edu/msu/pinkoski/project3_team16/DetailActivity.java
+++ app/src/main/java/edu/msu/pinkoski/project3_team16/DetailActivity.java
@@ -1,5 +1,6 @@
 package edu.msu.pinkoski.project3_team16;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -9,13 +10,14 @@ import android.view.View;
 
 public class DetailActivity extends AppCompatActivity{
 
-
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
 
     public void buttonMap(View view){
 
-
-
     }
 }
+
--- app/src/main/java/edu/msu/pinkoski/project3_team16/ResultActivity.java
+++ app/src/main/java/edu/msu/pinkoski/project3_team16/ResultActivity.java
@@ -1,7 +1,9 @@
 package edu.msu.pinkoski.project3_team16;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Savanna on 4/29/2017.
@@ -12,5 +14,10 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
+    }
+
+    public void onDetail(View view) {
+        Intent intent = new Intent(ResultActivity.this, DetailActivity.class);
+        startActivity(intent);
     }
 }
