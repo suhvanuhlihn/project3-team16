1de1aed9a410a1ab4e3bb3cacf62f8005e1c3a56
maliklau <malikl0799@gmail.com>
2017-04-29 14:44:52 -0400

    pushing info dialogue

--- app/src/main/java/edu/msu/pinkoski/project3_team16/MainActivity.java
+++ app/src/main/java/edu/msu/pinkoski/project3_team16/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.msu.pinkoski.project3_team16;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import static edu.msu.pinkoski.project3_team16.R.color.black;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSearch(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
+    }
+
+    public void onInfo(View view) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogHowTo = inflater.inflate(R.layout.dialogue_info, null);
+        dialogBuilder.setView(dialogHowTo);
+        dialogBuilder.setTitle("Welcome to MSU Parking Tips!");
+        dialogBuilder
+                .setCancelable(false)
+                .setNegativeButton("Done", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+        alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setTextColor(black);
     }
 }
--- app/src/main/java/edu/msu/pinkoski/project3_team16/ResultActivity.java
+++ app/src/main/java/edu/msu/pinkoski/project3_team16/ResultActivity.java
@@ -1,8 +1,16 @@
 package edu.msu.pinkoski.project3_team16;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Savanna on 4/29/2017.
  */
 
-public class ResultActivity {
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_activity);
+    }
 }
